46726f311000c1e5f0f85063835252089620d0fe
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-06-13 12:16:09 -0300

    chore:adiciona estrutura de pacotes

---
+++ src/main/java/com/clinicafisio/ClinicafisioApplication.java
@@ -0,0 +1,13 @@
+package com.clinicafisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicafisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicafisioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicafisio/ClinicafisioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicafisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicafisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
